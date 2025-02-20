fbfac3d7d24fe46866f68c2944e6ea0e07487f8d
spacedouut <73479844+notunk0wn23@users.noreply.github.com>
2025-02-20 16:45:57 -0500

    please work

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Cypher/', // Adjust if your repo has a different name
 })
