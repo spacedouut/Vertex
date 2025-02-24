fddc150c3ccdb828391569b75e83a65bcc09bfde
spacedouut <73479844+spacedouut@users.noreply.github.com>
2025-02-23 20:58:28 -0500

    Resolve some issues with previous PWA changes

--- vite.config.ts
+++ vite.config.ts
@@ -14,32 +14,32 @@ export default defineConfig({
         description: 'An AI client for the impatient',
         icons: [
           {
-            src: '/apple-touch-icon.png',
+            src: '/public/apple-touch-icon.png',
             sizes: '180x180',
             type: 'image/png',
           },
           {
-            src: '/favicon-32x32.png',
+            src: '/public/favicon-32x32.png',
             sizes: '32x32',
             type: 'image/png',
           },
           {
-            src: '/favicon-16x16.png',
+            src: '/public/favicon-16x16.png',
             sizes: '16x16',
             type: 'image/png',
           },
           {
-            src: '/favicon.ico',
+            src: '/public/favicon.ico',
             sizes: '48x48',
             type: 'image/x-icon',
           },
           {
-            src: '/icon-512.png',
+            src: '/public/icon-512.png',
             sizes: '512x512',
             type: 'image/png',
           },
           {
-            src: '/android-chrome-192x192.png',
+            src: '/public/android-chrome-192x192.png',
             sizes: '192x192',
             type: 'image/png',
           },
